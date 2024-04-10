a91aab520088c07ea9b1abfc51059aeaa2f2a8ce
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-04-10 20:37:50 -0300

    new code

---
+++ desafio18.py
@@ -0,0 +1,7 @@
+#arredondando números
+
+import math
+número = float(input('Digite um número decimal que irei te mostrar ele inteiro: '))
+númeroint = math.floor(número)
+print(f'O número escolhido foi {número}, ele transformado em inteiro é: {númeroint}')
+
