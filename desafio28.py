7008782073a477464b4d084f5a449b370e415f04
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-04-12 22:54:16 -0300

    Create desafio28.py

---
+++ desafio28.py
@@ -0,0 +1,7 @@
+#fatiamento básico, mas dessa vez tendo interação com usuário
+
+palavra = str(input('Diga uma palavra aleatória:'))
+num = int(input('Digite o número, que irei te mostrar a letra equivalente ao número: '))
+letra = palavra[num]
+
+print("A letra correspondente é:", letra)
