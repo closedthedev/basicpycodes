14d0e6cadba1ff825e4d770344980ba16f210466
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-04-12 23:22:59 -0300

    descobrindo comprimento de frase

---
+++ desafio30.py
@@ -0,0 +1,8 @@
+#descobrindo tamanho da frase
+
+import string
+
+frase = str(input('Digite uma frase aleatória: '))
+comprimento_frase = len(frase)
+
+print(f'A sua frase tem {comprimento_frase} caracteres!')
